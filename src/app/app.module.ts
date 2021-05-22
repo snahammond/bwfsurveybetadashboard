9c468d85e6a00018d99052479425163e6b433780
snahammond <niipad4g@gmail.com>
2021-05-22 14:55:10 +0200

    first commit dashboard

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SideNavOuterToolbarModule, SideNavInnerToolbarModule, SingleCardModule } from './layouts';
+import { FooterModule, LoginFormModule } from './shared/components';
+import { AuthService, ScreenService, AppInfoService } from './shared/services';
+import { UnauthenticatedContentModule } from './unauthenticated-content';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SideNavOuterToolbarModule,
+    SideNavInnerToolbarModule,
+    SingleCardModule,
+    FooterModule,
+    LoginFormModule,
+    UnauthenticatedContentModule,
+    AppRoutingModule
+  ],
+  providers: [AuthService, ScreenService, AppInfoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
