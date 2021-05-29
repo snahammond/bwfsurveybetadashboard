b910738a59c2d07278421d602457d3b00699b56d
snahammond <niipad4g@gmail.com>
2021-05-29 19:58:17 +0200

    initial survey data grid

--- src/app/pages/initial-survey/initial-survey.component.ts
+++ src/app/pages/initial-survey/initial-survey.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { APIService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-initial-survey',
   templateUrl: './initial-survey.component.html',
   styleUrls: ['./initial-survey.component.scss']
 })
-export class InitialSurveyComponent implements OnInit {
+export class InitialSurveyComponent {
+  dataSource: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private api: APIService) { 
+    this.api.ListInitialSurveys().then(event => {      
+      console.log('Length of initial surveys ' + event.items.length);
+      this.dataSource = event.items;
+    }).catch(e => {
+      console.log('error initialSurveys...', e);
+    });
   }
+
+  
 
 }
